e50bbb71bf638fa8aac7a855525fa158f4734e7c
Sheraz Akram <sherazakram.pro@gmail.com>
2024-04-21 14:59:05 +0500

    setup static files

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 require("dotenv").config()
 const express = require("express")
+const path = require("path")
+
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, "public")))
 app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
